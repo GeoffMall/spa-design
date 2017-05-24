0dbef6c57b7ac63d48c56ada1e2a2b73d94312a6
Geoff <pizzintea@hotmail.com>
2017-05-24 02:46:39 -0700

    adding product mock data, color scheme change;

--- frontend/src/app/models/product-mock.ts
+++ frontend/src/app/models/product-mock.ts
@@ -2,156 +2,49 @@ import {Product} from './product';
 export const PRODUCTS: Product[] = [
   {
     id: 1,
-    name: 'BODY MASSAGE SCRUB',
-    avatar: 'Avatar 1',
-    price: '$130.00',
-    description: 'Feeling a little rough or a little dry? Rejuvenate your skin with this full-body exfoliation and ' +
-    'hydration treatment. A body scrub treatment will give your skin a smooth, fresh, healthy glow that lasts for ' +
-    'weeks. It lifts away the old lifeless skin cells to make room for that vital new layer while stimulating lymph ' +
-    'fluids that will help in the removal of toxins in your body. Great before special events, this treatment is a ' +
-    'completely natural way to look and feel your best.Follow up your body scrub treatment with a massage at our ' +
-    'Imperial Spa for an extra special experience. Your therapist can help relax and unwind with one of our massage ' +
-    'treatments following your body scrub.',
+    name: 'SkinMedica Ultimate Daily SPF20',
+    avatar: 'assets/img/mock-product-1.jpg',
+    price: '$88.00',
+    description: 'APPROPRIATE FOR: Normal or combination skin\n ' +
+    'PURPOSE: Long lasting hydration and broad spectrum UVA/UVB protection \n ' +
+    'WHEN TO APPLY: In the morning after cleansing, toning and application of treatment products \n ' +
+    'WHERE TO APPLY: Face, neck and other sun exposed areas \n ' +
+    'HOW TO APPLY: Apply liberally to face (neck and chest if desired) in the morning or before sun exposure. ' +
+    'Reapply as needed. Children under 6 months of age: ask a doctor.',
   },
   {
     id: 2,
-    name: 'ALGAE BODY MASSAGE SCRUB',
-    avatar: 'Avatar 2',
-    price: '$160.00',
-    description: 'Algae Body Scrub is a unique, luxury scented body treatment with ultra-polishing seaweed (algae) ' +
-    'power. Algae Body Scrub gently removes dead skin cells and impurities while enhancing detoxification and ' +
-    'increasing blood flow. This exclusive natural body exfoliant acts as a detoxifying and remineralizing treatment, ' +
-    'improving uneven skin texture, luck of radiance and dullness and leaving your skin silky soft and delicately ' +
-    'perfumed.',
+    name: 'SkinMedica AHA/BHA Exfoliating Cleanser',
+    avatar: 'assets/img/mock-product-2.jpg',
+    price: '$45.00',
+    description: 'APPROPRIATE FOR: All skin types \n ' +
+    'PURPOSE: Improve the appearance of skin tone, texture and smoothness. \n ' +
+    'RESULTS: Enhanced skin radiance and luminosity \n ' +
+    'Improved appearance of skin texture, tone and smoothness through mild exfoliation \n' +
+    'WHEN TO APPLY:  First product used in your morning and evening skin care routine, as tolerated \n' +
+    'WHERE TO APPLY:  Face (neck and chest if desired)'
   },
   {
     id: 3,
-    name: 'AROMA BODY MASSAGE SCRUB',
-    avatar: 'Avatar 3',
-    price: '$160.00',
-    description: 'This exceptional and luxurious body scrub treatment is formulated to exfoliate, hydrate, nourish ' +
-    'and detoxify the body with the healing benefits of aromatherapy. A body scrub is followed by an aromatherapy ' +
-    'massage enhanced with an aroma of natural ingredients and hydrating serum utilizing soothing and comforting ' +
-    'massage strokes. You\'ll be guided to ultimate state of relaxation.',
+    name: 'Regenica Repair Complex',
+    avatar: 'assets/img/mock-product-3.jpg',
+    price: '$150.00',
+    description: 'Regenica® Repair Complex helps accelerate skin renewal. Made with MRCx™ next generation growth ' +
+    'factor technology, it’s specifically formulated for sensitive skin. Help restore your skin to its optimal health.',
   },
   {
     id: 4,
-    name: 'SALT BODY MASSAGE SCRUB',
-    avatar: 'Avatar 4',
-    price: '$160.00',
-    description: 'Feel stress melt away as your skin is gently exfoliated and massaged using a blend of rich mineral ' +
-    'and therapeutic sea salts to gently and effectively awaken circulation while removing accumulated residues and ' +
-    'impurities. As invigorating as sea water, this treatment allows the inner, younger, softer, layers of skin to ' +
-    'emerge, leaving skin polished, amazingly smooth and radiant.',
-  },
-  {
-    id: 5,
-    name: 'EUROPEAN CLEANSING FACIAL',
-    avatar: 'Avatar 5',
-    price: '$100.00+',
-    description: 'A deep cleansing facial incorporating classic european movements. (ask about upgrades on customized care)',
-  },
-  {
-    id: 6,
-    name: 'VITAMIN C WHITE EFFECT',
-    avatar: 'Avatar 5',
-    price: '$139.00',
-    description: 'Achieve a brighter and more harmonious complexion with tropical plant extracts and vitamin C. ' +
-    'Reverse past sun damage, uneven skin or age spots and achieve soft smooth even looking skin with the triple action ' +
-    'White Effect.',
-  },
-  {
-    id: 7,
-    name: 'ECOLLAGEN BOOSTER TREATMENT',
-    avatar: 'Avatar 5',
-    price: '$139.00',
-    description: 'Builds up and firms skin structure with the Collagen Booster, Collagen Biomatrix and through manual ' +
-    'stimulation of connective tissue.',
-  },
-  {
-    id: 8,
-    name: 'INTENSIVE FIRMING & HYDRATING',
-    avatar: 'Avatar 5',
-    price: '$139.00',
-    description: 'Receive a refreshing mask, rich in active ingredients for all skin types, resulting in visibly ' +
-    'smoothing appearance. Protects, firms, and hydrates the skin with the proven effectiveness of algae.',
-  },
-  {
-    id: 9,
-    name: 'THERMO ALGAE CLAYFACIAL',
-    avatar: 'Avatar 5',
-    price: '$139.00',
-    description: 'Builds up and firms skin structure with the Collagen Booster, Collagen Biomatrix and through manual ' +
-    'stimulation of connective tissue.',
-  },
-  {
-    id: 10,
-    name: 'ECOLLAGEN BOOSTER TREATMENT',
-    avatar: 'Avatar 5',
-    price: '$139.00',
-    description: 'A 4 step comprehensive process of using a rare algae mask followed by a firming warm clay mask.' +
-    ' Used to calm and hydrate for immediate results.',
-  },
-  {
-    id: 11,
-    name: 'CLEAR ACTION FOR ACNE',
-    avatar: 'Avatar 5',
-    price: '$139.00',
-    description: 'Smoothes and relieves troubled skin to prevent further breakouts and gain control of your skin. ' +
-    'This specialized formula addresses different levels and grades of acne. With a thorough analysis and proper ' +
-    'application of the treatment, we can get your skin to where it should be.',
-  },
-  {
-    id: 12,
-    name: 'GENTLEMEN\'S FACIAL',
-    avatar: 'Avatar 5',
-    price: '$125.00',
-    description: 'Renew strength and vitality in your skin with a custom tailored gentlemen\'s facial.',
-  },
-  {
-    id: 13,
-    name: 'BLACK DIAMOND FACIAL / ACTIVE VITALIZING',
-    avatar: 'Avatar 5',
-    price: '$160.00',
-    description: 'This luxurious fruit treatment actively works to bring out the vitality and youthful appearance ' +
-    'in your skin involving maximum exfoliation and revitalization for immediate results.',
-  },
-  {
-    id: 14,
-    name: 'ADVANCED BIOGEN ROSE QUARTZ BIO- ENERGY TREATMENT',
-    avatar: 'Avatar 5',
-    price: '$160.00',
-    description: 'Rose Quartz, the stone of love and the heart, transfers healing energy and improves skin\'s ' +
-    'vital function. \n Alleviates dryness and dehydration \n Promotes cell renewal \n Stimulates skin\'s ' +
-    'metabolism \n Improves elasticity',
-  },
-  {
-    id: 15,
-    name: 'ENERGIZING OXYGEN TREATMENT',
-    avatar: 'Avatar 5',
-    price: '$175.00',
-    description: 'This stimulates energy levels allowing skin to breathe instantly: contains pure, encapsulated ' +
-    'oxygen which is released directly to the cells. Tired, stressed skin instantly receives new energy, enabling it ' +
-    'to breathe again, while restoring the skin\'s fresh, vitalized and radiant appearance.',
-  },
-  {
-    id: 16,
-    name: 'HSR : THE LIFT EXPERIENCE',
-    avatar: 'Avatar 5',
-    price: '$185.00',
-    description: 'Effectively protects and stimulates the skin\'s natural anti-aging system with the active HSR ' +
-    'lifting-complex, creating an innovative 4-dimentional lifting effect. Leaves skin intensively moisturized and ' +
-    'prevents the formation of lines and wrinkles.',
-  },
-  {
-    id: 17,
-    name: 'SEA CREATION: SUBMERGE INTO DEEP WATERS',
-    avatar: 'Avatar 5',
-    price: '$305.00',
-    description: 'The volcanic rifts on the seabed in Guayamas Bay are amongst the most eco-rich regions with high ' +
-    'energy active substances. This luxuriant treatment uses those rich and abundant ingredients to give you a chance ' +
-    'to experience the concentrated powers of the deep sea. For eternally beautiful skin, a special complex ' +
-    'formulated with pearl proteins and sea silk are incorporated with the minerals from the shells to work wonders. ' +
-    'Each jar is personally filled for you, immerse yourself in an ocean of luxury.',
+    name: 'SkinMedica Retinol 0.25',
+    avatar: 'assets/img/mock-product-4.jpg',
+    price: '$65.00',
+    description: 'APPROPRIATE FOR: All skin types \n ' +
+    'PURPOSE: Enhance skin tone, texture, and resiliency \n ' +
+    'RESULTS: \n ' +
+    'Diminished appearance of fine lines and coarse wrinkles \n ' +
+    'Enhanced skin tone and texture \n ' +
+    'WHEN TO APPLY:  In the evening after cleansing and toning and before applying your moisturizer. If a retinol ' +
+    'product has never been used before, begin twice a week 2-3 days apart, increasing frequency slowly until daily ' +
+    'use is tolerated. \n' +
+    'WHERE TO APPLY:  Face (neck and chest if desired) .',
   },
 ];
